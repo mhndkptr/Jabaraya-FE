a9cfa3c6d2c31b3d08005b5ed54b30f9b9283d88
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-07-17 18:22:23 +0700

    feat: Add NavbarAdmin component for admin dashboard

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AdminLayout({ children }) {
   return (
     <>
-      <div>AdminLayout</div>
+      {/* <div>AdminLayout</div> */}
       {children}
     </>
   );
